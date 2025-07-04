8ade92ce079410f708fb2ad61f62d8c0f117c6a0
Jairo Allende <jairoalle23@gmail.com>
2025-07-03 22:42:42 -0300

    se agrego la parte del dni

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function toggleTheme() {
-    const html = document.documentElement;
-    const icon = document.getElementById('icono-tema');
-
-    const isDark = html.dataset.theme == "dark";
-    html.dataset.theme = isDark ? "ligth" : "dark";
-
-    icon.className = isDark ? "bi bi-moon-fill" : "bi bi-sun-fill";
-}
