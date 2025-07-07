053e0df5a19f8f9814de0aaa538786a960eb094e
Jairo Allende <jairoalle23@gmail.com>
2025-07-07 18:25:50 -0300

    Se creo la smartcard

--- js/home.js
+++ js/home.js
@@ -1,3 +1,19 @@
 document.getElementById("home-pedir_turno").addEventListener("click" ,()=>{
     window.location.href = "/html/filtros_turnos.html";
 })
+
+document.getElementById("home-smartcard").addEventListener("click" ,()=>{
+    window.location.href = "/html/smartcard.html";
+})
+
+document.getElementById("arrow_down").addEventListener("mouseenter" , () => {
+    document.getElementById("opciones-mi_perfil").style.display="flex"
+})
+
+document.getElementById("mi-perfil").addEventListener("mouseleave" , () => {
+    document.getElementById("opciones-mi_perfil").style.display="none"
+})
+
+document.getElementById("opciones-mi_perfil").addEventListener("mouseleave" , () => {
+    document.getElementById("opciones-mi_perfil").style.display="none"
+})
