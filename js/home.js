8f54d09cc04910f515e0fa4807ec954f68bcadd7
Jairo Allende <jairoalle23@gmail.com>
2025-07-15 10:56:51 -0300

    cambie las rutas de home.js

--- js/home.js
+++ js/home.js
@@ -1,25 +1,25 @@
-document.getElementById("home-pedir_turno").addEventListener("click" ,()=>{
-    window.location.href = "/html/filtros_turnos.html";
-})
+document.getElementById("home-pedir_turno").addEventListener("click", () => {
+    window.location.href = "filtros_turnos.html";
+});
 
-document.getElementById("home-smartcard").addEventListener("click" ,()=>{
-    window.location.href = "/html/smartcard.html";
-})
+document.getElementById("home-smartcard").addEventListener("click", () => {
+    window.location.href = "smartcard.html";
+});
 
-document.getElementById("home-examenes_clinicos").addEventListener("click" ,()=>{
-    window.location.href = "/html/examenes-clinicos.html";
-})
+document.getElementById("home-examenes_clinicos").addEventListener("click", () => {
+    window.location.href = "examenes-clinicos.html";
+});
 
-document.getElementById("arrow_down").addEventListener("mouseenter" , () => {
-    document.getElementById("opciones-mi_perfil").style.display="flex"
-})
+document.getElementById("arrow_down").addEventListener("mouseenter", () => {
+    document.getElementById("opciones-mi_perfil").style.display = "flex";
+});
 
-document.getElementById("mi-perfil").addEventListener("mouseleave" , () => {
-    document.getElementById("opciones-mi_perfil").style.display="none"
-})
+document.getElementById("mi-perfil").addEventListener("mouseleave", () => {
+    document.getElementById("opciones-mi_perfil").style.display = "none";
+});
 
-document.getElementById("opciones-mi_perfil").addEventListener("mouseleave" , () => {
-    document.getElementById("opciones-mi_perfil").style.display="none"
-})
+document.getElementById("opciones-mi_perfil").addEventListener("mouseleave", () => {
+    document.getElementById("opciones-mi_perfil").style.display = "none";
+});
 
 
