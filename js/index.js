882d624afad72b236ec72ca248632ed7d9f38b84
Jairo Allende <jairoalle23@gmail.com>
2025-07-15 09:48:43 -0300

    cambie las rutas para acceder al login

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-botonIniciarSesion = document.getElementById('opciones-iniciar_sesion');
-cardPlan = document.getElementsByClassName('card-plan');
+const botonIniciarSesion = document.getElementById('opciones-iniciar_sesion');
+const cardPlan = document.getElementsByClassName('card-plan');
 
 botonIniciarSesion.addEventListener("click", () =>{
-    window.location.href = "../html/login.html";
+    window.location.href = "html/login.html";
 });
 
 Array.from(cardPlan).forEach(element => {
   element.addEventListener("click", () => {
-    window.open("/html/plan_silver.html", "_blank")
+    window.open("html/plan_silver.html", "_blank");
   });
 });
 
 document.getElementById("container-socios").addEventListener("click", () => {
-  window.location.href="../html/login.html";
-})
+  window.location.href = "html/login.html";
+});
